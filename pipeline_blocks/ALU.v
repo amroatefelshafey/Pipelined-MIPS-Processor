23966479ad8b95c6fbdd410531c6aa1678fa1e08
amroatefelshafey <amroatefelshafey@gmail.com>
2026-04-03 00:13:11 +0400

    Refactor ALU module to update output assignments

--- pipeline_blocks/ALU.v
+++ pipeline_blocks/ALU.v
@@ -12,29 +12,30 @@ wire [31:0] Sum, Shift_Result, OR_Result;
 wire [63:0] Mult_Result;
 
 assign Start = ~ALUCtl[1] & ~ALUCtl[0];
+assign RST = ~Start; //RST (active-low) clears the multiplers internal state back to 0. Basically, ensures new operation starts
 assign M = ALUCtl[2];
 
 assign Shift_Result = B << A; // B is shifted because A is rs, and sll does R[rd] = R[rt] << shamt (B is rt, A is shamt)
 assign OR_Result = A | B;
 Adder_Subtractor Adder_Subtractor(A, B, M, Sum, Carry);
-Multiplier Seq_Multiplier(CLK, RST, Start, A, B, Mult_OUT);
+Multiplier Seq_Multiplier(CLK, RST, Start, A, B, Mult_Result, //There must the "Ready" here);
 
 always@(*)
 begin
 
 	casex(ALUCtl)
 	
-		3'bx00: {Hi_OUT, Lo_OUT} = Mult_Result;
-		3'bx01: Lo_OUT = OR_Result;
-		3'bx10: Lo_OUT = Sum;
-		3'bx11: Lo_OUT = Shift_Result;
-		default: {Hi_OUT, Lo_OUT} = 32'bx;
+		3'bx00: Mult_OUT = Mult_Result;
+		3'bx01: OUT = OR_Result;
+		3'bx10: OUT = Sum;
+		3'bx11: OUT = Shift_Result;
+		default: OUT = 32'bx;
 		
 	endcase
 end
 
 assign Carry = (ALUCtl == 3'bx10) ? c : 0;
-assign Zero = ~|Lo_OUT;
-assign Sign = Lo_OUT[31];
+assign Zero = ~|OUT;
+assign Sign = OUT[31];
 
 endmodule
