06a630ddaf46c8e028a8e14d82b5e167a4dc5fe8
amroatefelshafey <amroatefelshafey@gmail.com>
2026-04-09 21:40:30 +0400

    Enhance hazard unit for improved data hazard management
    Updated hazard unit to include IDEXrd and Branch signal for better hazard detection and handling.

--- pipeline_blocks/hazard_unit.v
+++ pipeline_blocks/hazard_unit.v
@@ -1,13 +1,14 @@
 module hazard_unit(
   input [4:0] IFIDrs, IFIDrt,
   input [4:0] IDEXrs, IDEXrt,
+  input [4:0] IDEXrd, // Output of the Multiplexer choosing between rd, rt, or 31
   input [4:0] EXMEMrd,
   input [4:0] MEMWBrd,
   
-  input EXMEMRegWrite, MEMWBRegWrite, // Conditions for data hazards (if no write, there is no hazard!)
+  input IDEXRegWrite, EXMEMRegWrite, MEMWBRegWrite, // Conditions for data hazards (if no write, there is no hazard!)
   input IDEXMemRead, EXMEMMemRead, MEMWBMemRead, // These deal with load-use and load-store hazards respectively
 
-  input PCSrc, Jump, // Necessary Signals to determine whether we flush or not
+  input PCSrc, Jump, Branch // Necessary Signals to determine whether we flush/stall or not. Branch is used only to determine stalling
   
   output [1:0] ForwardA, 
   output reg [1:0] ForwardB, // Forwarding signals
@@ -17,8 +18,8 @@ module hazard_unit(
 );
 
 
-  wire loadFollowingBranch = (IDEXMemRead | EXMEMMemRead) & PCSrc;
-  wire EXFollowingBranch = EXMEMRegWrite & (EXMEMrd != 0) & (IFIDrs  | IFIDrt);
+  wire loadFollowingBranch = EXMEMMemRead & ( (EXMEMrd == IFIDrs) | (EXMEMrd == IFIDrt) ) & Branch ; // This deals with load-branch 2nd stall
+  wire EXFollowingBranch = IDEXRegWrite & (IDEXrd != 0) & ( (IFIDrs == IDEXrd) | (IFIDrt == IDEXrd) ) & Branch;
   // DATA HAZARD HANDLING
   // Forwarding Logic (ForwardA & ForwardB)
 
@@ -42,7 +43,7 @@ module hazard_unit(
   assign ForwardDM = MEMWBMemRead & (MEMWBrd == EXMEMrd);
   
   // ----- Load-Use Stalling Logic (Stall, IFIDWrite, PCWrite) -----
-  assign Stall = (IDEXMemRead & ( (IDEXrt == IFIDrs) | (IDEXrt == IFIDrt) ) | )& !Jump; // Must assert the instruction is not a jump 
+  assign Stall = (IDEXMemRead & ( (IDEXrt == IFIDrs) | (IDEXrt == IFIDrt) ) | loadFollowingBranch ) & !Jump; // Must assert the instruction is not a jump 
                                                                                     // (jump signal here is from cycle 2)
   // CONTROL HAZARD HANDLING
 
