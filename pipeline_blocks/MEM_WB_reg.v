f18608f8b7976eb590eb73d427e3f8b2fee34597
Abdelrahman Mansour <abdelrahmanamm2005@gmail.com>
2026-04-11 19:57:30 +0400

    Update MEM_WB_reg.v

--- pipeline_blocks/MEM_WB_reg.v
+++ pipeline_blocks/MEM_WB_reg.v
@@ -2,27 +2,27 @@ module MEM_WB_reg(
     input clk,
 
     //  Control signals IN 
-    input        in_MemtoReg, in_RegWrite, in_SLT, in_HiLoWrite,
+    input        in_MemtoReg, in_RegWrite, in_SLT,
 
     //  Data going IN 
-    input [31:0] in_ALU_Hi, in_ALU_Lo,
+    input [31:0] in_ALU
     input [31:0] in_Read_Data,
     input [4:0]  in_Write_Reg,
-    input        in_Sign,
+    input [31:0] in_Sign,
 
     //  Control signals OUT 
-    output reg        out_MemtoReg, out_RegWrite, out_SLT, out_HiLoWrite,
+    output reg        out_MemtoReg, out_RegWrite, out_SLT,
 
     //  Data going OUT 
-    output reg [31:0] out_ALU_Hi, out_ALU_Lo,
+    output reg [31:0] out_ALU
     output reg [31:0] out_Read_Data,
     output reg [4:0]  out_Write_Reg,
-    output reg        out_Sign
+    output reg [31:0] out_Sign
 );
     always @(posedge clk) begin
         out_MemtoReg  <= in_MemtoReg;  out_RegWrite  <= in_RegWrite;
-        out_SLT       <= in_SLT;       out_HiLoWrite <= in_HiLoWrite;
-        out_ALU_Hi    <= in_ALU_Hi;    out_ALU_Lo    <= in_ALU_Lo;
+        out_SLT       <= in_SLT;      
+        out_ALU       <= in_ALU 
         out_Read_Data <= in_Read_Data;
         out_Write_Reg <= in_Write_Reg;
         out_Sign      <= in_Sign;
