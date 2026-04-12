cd9a95f99f3d93dc1645bc87c07e67bf08811db4
amroatefelshafey <amroatefelshafey@gmail.com>
2026-04-12 13:49:24 +0400

    Added Mult_READY signal and improved readability

--- pipeline_blocks/ALU.v
+++ pipeline_blocks/ALU.v
@@ -4,38 +4,35 @@ module ALU(
 			input [2:0] ALUCtl,
 			output reg [31:0] OUT,
 			output reg [63:0] Mult_OUT,
-			output Carry, Zero, Sign
+			output Sign, Mult_READY
 			);
 
-wire M, c;
-wire [31:0] Sum, Shift_Result, OR_Result;
-wire [63:0] Mult_Result;
+	wire M, c;
+	wire [31:0] Sum, Shift_Result, OR_Result;
+	wire [63:0] Mult_Result;
 
-assign Start = ~ALUCtl[1] & ~ALUCtl[0];
-assign RST = ~Start; //RST (active-low) clears the multiplers internal state back to 0. Basically, ensures new operation starts
-assign M = ALUCtl[2];
+	assign Start = ~ALUCtl[1] & ~ALUCtl[0];
+	assign RST = ~Start; //RST (active-low) clears the multiplers internal state back to 0. Basically, ensures new operation starts
+	assign M = ALUCtl[2];
 
-assign Shift_Result = B << A; // B is shifted because A is rs, and sll does R[rd] = R[rt] << shamt (B is rt, A is shamt)
-assign OR_Result = A | B;
-Adder_Subtractor Adder_Subtractor(A, B, M, Sum, Carry);
-Multiplier Seq_Multiplier(CLK, RST, Start, A, B, Mult_Result, //There must the "Ready" here);
+	assign Shift_Result = B << A; // B is shifted because A is rs, and sll does R[rd] = R[rt] << shamt (B is rt, A is shamt)
+	assign OR_Result = A | B;
+	Adder_Subtractor Adder_Subtractor(A, B, M, Sum, Carry);
+	Seq_Multiplier Multiplier(CLK, RST, Start, A, B, Mult_Result, Mult_READY);
 
-always@(*)
-begin
+	always@(*) begin
 
-	casex(ALUCtl)
-	
+		casex(ALUCtl)
+		
 		3'bx00: Mult_OUT = Mult_Result;
 		3'bx01: OUT = OR_Result;
 		3'bx10: OUT = Sum;
 		3'bx11: OUT = Shift_Result;
 		default: OUT = 32'bx;
 		
-	endcase
-end
-
-assign Carry = (ALUCtl == 3'bx10) ? c : 0;
-assign Zero = ~|OUT;
-assign Sign = OUT[31];
+		endcase
+	end
+	
+	assign Sign = OUT[31];
 
 endmodule
